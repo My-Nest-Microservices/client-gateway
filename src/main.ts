2aadd92ccc48be954669d4a2c9f9549cdb3cfea6
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-09-19 18:13:00 +0200

    feat: .env with port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const logger = new Logger('ClientGateway-main');
+
+  await app.listen(envs.port);
+
+  logger.log(`Application is running on: ${envs.port}`);
 }
 bootstrap();
