c037ffbbc766831922110539a74ab8a66cee4ddf
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-09-19 18:28:59 +0200

    feat: inject PRODUCT_SERVICE TCP transport

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 
 import { ProductsController } from './products.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PRODUCT_SERVICE } from 'src/config';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: PRODUCT_SERVICE,
+        transport: Transport.TCP,
+      },
+    ]),
+  ],
 })
 export class ProductsModule {}
