bb10e70adfb717d410fbaf63e8acf9306478fb20
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-10-13 10:24:27 +0200

    feat: dockerfile and improved custom exception filter

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,11 +5,11 @@ import { PRODUCT_SERVICE } from './services';
 interface EnvVars {
   PORT: number;
 
-  PRODUCTS_MICROSERVICE_HOST: string;
-  PRODUCTS_MICROSERVICE_PORT: number;
+  // PRODUCTS_MICROSERVICE_HOST: string;
+  // PRODUCTS_MICROSERVICE_PORT: number;
 
-  ORDERS_MICROSERVICE_HOST: string;
-  ORDERS_MICROSERVICE_PORT: number;
+  // ORDERS_MICROSERVICE_HOST: string;
+  // ORDERS_MICROSERVICE_PORT: number;
 
   NATS_SERVERS: string[];
 }
@@ -17,11 +17,11 @@ interface EnvVars {
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
-    PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
-    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
+    // PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
+    // PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
 
-    ORDERS_MICROSERVICE_HOST: joi.string().required(),
-    ORDERS_MICROSERVICE_PORT: joi.string().required(),
+    // ORDERS_MICROSERVICE_HOST: joi.string().required(),
+    // ORDERS_MICROSERVICE_PORT: joi.string().required(),
 
     NATS_SERVERS: joi.array().items(joi.string().required()).required(),
   })
@@ -40,11 +40,11 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
-  productsHost: envVars.PRODUCTS_MICROSERVICE_HOST,
-  productsPort: envVars.PRODUCTS_MICROSERVICE_PORT,
+  // productsHost: envVars.PRODUCTS_MICROSERVICE_HOST,
+  // productsPort: envVars.PRODUCTS_MICROSERVICE_PORT,
 
-  ordersHost: envVars.ORDERS_MICROSERVICE_HOST,
-  ordersPort: envVars.ORDERS_MICROSERVICE_PORT,
+  // ordersHost: envVars.ORDERS_MICROSERVICE_HOST,
+  // ordersPort: envVars.ORDERS_MICROSERVICE_PORT,
 
   natsServers: envVars.NATS_SERVERS,
 };
