d04378165c298061e47da9554e19b5388a9a5c0b
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-10-13 11:38:52 +0200

    remove test console.log

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,6 @@ async function bootstrap() {
 
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
-  console.log('Segundo cambio');
-
   await app.listen(envs.port);
 
   logger.log(`Application is running on: ${envs.port}`);
