cbf30920ae141086f0e85ecc5d5382956aa6f8d1
Jose Alberto Delgado Robles <jdelgadorobles84@gmail.com>
2024-10-15 17:12:58 +0200

    refactor: removed unused environment variables from evns config

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,25 +4,12 @@ import { PRODUCT_SERVICE } from './services';
 
 interface EnvVars {
   PORT: number;
-
-  // PRODUCTS_MICROSERVICE_HOST: string;
-  // PRODUCTS_MICROSERVICE_PORT: number;
-
-  // ORDERS_MICROSERVICE_HOST: string;
-  // ORDERS_MICROSERVICE_PORT: number;
-
   NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
-    // PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
-    // PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
-
-    // ORDERS_MICROSERVICE_HOST: joi.string().required(),
-    // ORDERS_MICROSERVICE_PORT: joi.string().required(),
-
     NATS_SERVERS: joi.array().items(joi.string().required()).required(),
   })
   .unknown(true);
@@ -40,11 +27,7 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
-  // productsHost: envVars.PRODUCTS_MICROSERVICE_HOST,
-  // productsPort: envVars.PRODUCTS_MICROSERVICE_PORT,
-
-  // ordersHost: envVars.ORDERS_MICROSERVICE_HOST,
-  // ordersPort: envVars.ORDERS_MICROSERVICE_PORT,
-
   natsServers: envVars.NATS_SERVERS,
 };
+
+
